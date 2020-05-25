95c07445a2a64f6e525b32c3b634bf35c4529ca9
wallwriting <hwang.john@gmail.com>
2020-05-25 14:44:40 -0400

    New upsert file
    Allows for situations where there are no dml indicators and/or no version ids.

--- upsert_sp.sql
+++ upsert_sp.sql
@@ -1,13 +1,16 @@
 CREATE OR REPLACE PROCEDURE upsert_sp(target_table INOUT character varying, target_key INOUT character varying, target_version INOUT character varying, source_table INOUT character varying, source_key INOUT character varying, source_version INOUT character varying, source_dml_indicator INOUT character varying)
---drop procedure PROCEDURE upsert_sp_test(target_table INOUT character varying, target_key INOUT character varying, target_version INOUT character varying, source_table INOUT character varying, source_key INOUT character varying, source_version INOUT character varying, source_dml_indicator INOUT character varying)
+--drop procedure PROCEDURE upsert_sp(target_table INOUT character varying, target_key INOUT character varying, target_version INOUT character varying, source_table INOUT character varying, source_key INOUT character varying, source_version INOUT character varying, source_dml_indicator INOUT character varying)
+ 
 AS $$
 DECLARE
-    insert_var varchar(20);
-    update_var varchar(20);
+    insert_var character varying;
+    update_var character varying;
     col_nm_cursor record;
-    col_txt_var varchar(100);
+    col_txt_var character varying;
     colArrayVar character varying;
     colArrayPrefixVar character varying;
+    finishInsertVar character varying;
+    finishDeleteVar character varying;
 BEGIN
     /*creates the temp table that will hold the column list*/
     drop table if exists tmp_column_name_list_table_4383acad9Dk2;
@@ -52,9 +55,9 @@ BEGIN
                               /*skips the blank column name in the first pass*/
                               WHEN test_col = '' 
                                 /*string function removes the leading and trailing parentheses*/
-                                THEN 'src.' || substring(col_txt_var, 2, (select len(col_txt_var) - 2)) 
+                                THEN 'src.'||substring(col_txt_var, 2, (select len(col_txt_var) - 2)) 
                                 /*adds a comma in between the values*/
-                                ELSE test_col || ', ' || 'src.' || substring(col_txt_var, 2, (select len(col_txt_var) - 2)) 
+                                ELSE test_col || ', ' || 'src.'||substring(col_txt_var, 2, (select len(col_txt_var) - 2)) 
                           END;
         END LOOP;
 
@@ -62,10 +65,26 @@ BEGIN
         /*a second version of the column list that removes the table prefix*/
         colArrayVar = (SELECT REPLACE(colArrayPrefixVar, 'src.', ''));
 
+
+/*This ends the column list generating section*/
+
 insert_var = 'I';
 update_var = 'U';
 
-/*This ends the column list generating section*/
+/*for the end of the insert statement, this will create different lines based on
+whether the call passed a real dml indicator or a value of X*/
+IF UPPER(source_dml_indicator) = 'X' THEN finishInsertVar = '1=1';
+    ELSE finishInsertVar = 'src.' || source_dml_indicator || ' IN(' || quote_literal(insert_var) || ', ' || quote_literal(update_var) || ')';
+END IF;
+
+/*for the end of the delete statement, this will create different lines based on
+whether the call passed a real version id or the same value as the key*/
+IF target_version = target_key THEN finishDeleteVar = '1=1';
+    ELSIF source_version = source_key THEN finishDeleteVar = '1=1';
+    ELSE finishDeleteVar = 'src.'||source_version || ' >= maxtgt.tgt_version_col';
+END IF;
+
+
 
 /*This starts the actual delete and insert*/
 EXECUTE
@@ -75,18 +94,17 @@ FROM
 	 ' || target_table || 
 ' WHERE 
 	 ' || target_key || ' IN
-    /*gets target table_key that have a 
-    version number lower than the source*/
+    /*gets target table_key that have a version number lower than the source*/
     (
         SELECT
-            maxtgt.' || target_key ||
+            maxtgt.'||target_key ||
         ' FROM
             /*target table id and max version number*/
             (
                 SELECT
                      ' || target_key || ',
-                    MAX(' || target_version || ') AS ' || target_version ||
-                ' FROM
+                    MAX(' || target_version || ') AS tgt_version_col 
+                FROM
                      ' || target_table ||
                 ' GROUP BY
                      ' || target_key ||
@@ -95,12 +113,12 @@ FROM
             /*source table*/
              ' || source_table || ' src
         ON 
-            src.' || source_key || ' = maxtgt.' || target_key ||
-            '/*only gets rows where the source
-            timestamp is greater than the
-            targets timestamp*/
-            AND src.' || source_version || ' >= maxtgt.' || target_version ||
-    ' )
+            src.'||source_key || ' = maxtgt.'||target_key ||
+            '/*only gets rows where the source timestamp is greater than the target timestamp*/
+            AND ' || 
+            finishDeleteVar
+--'src.'||source_version || ' >= maxtgt.tgt_version_col'
+    ||' )
 ;'
 ;
 EXECUTE
@@ -123,27 +141,28 @@ INSERT INTO ' || target_table ||
 				/*the record identifier*/
           		  ' || target_key || ', 
 				/*most recent version*/
-          		MAX(' || target_version || ') AS ' || target_version || 
-          	' FROM '
-          		 || source_table ||
-			' WHERE 
-          		/*DML indcator, must filter out
-          		deletes prior to the GROUP BY*/
-          		  ' || source_dml_indicator || ' IN(' || quote_literal(insert_var) || ', ' || quote_literal(update_var) || ') 
-          	GROUP BY '
+          		MAX(' || target_version || ') AS tgt_version_col  
+          	FROM '
+          		 || source_table || ' src 
+			 WHERE 
+          		/*DML indcator, must filter out deletes prior to the GROUP BY*/ ' ||
+              finishInsertVar
+--                'src.'||source_dml_indicator || ' IN(' || quote_literal(insert_var) || ', ' || quote_literal(update_var) || ')'
+          	|| ' GROUP BY '
           		 || target_key || 
         ') mx
-        ON src.' || source_key || ' = mx.' || target_key ||
-        ' AND src.' || source_version || ' = mx.' || target_version ||
-    ' LEFT JOIN '
+        ON src.'||source_key || ' = mx.'||target_key ||
+        ' AND src.'||source_version || ' = mx.tgt_version_col 
+    LEFT JOIN '
          || target_table || ' tgt 
-        ON src.' || source_key || ' = tgt.' || target_key || 
-        ' AND src.' || source_version || ' = tgt.' || target_version || 
+        ON src.'||source_key || ' = tgt.'||target_key || 
+        ' AND src.'||source_version || ' = tgt.'||target_version || 
     ' WHERE 
         /*only inserts values not already present in target*/
-        tgt.' || target_key || ' IS NULL
-        AND src.' || source_dml_indicator || ' IN(' || quote_literal(insert_var) || ', ' || quote_literal(update_var) || ') 
-    ;'
+        tgt.'||target_key || ' IS NULL AND ' ||
+        finishInsertVar
+--        'src.'||source_dml_indicator || ' IN(' || quote_literal(insert_var) || ', ' || quote_literal(update_var) || ')'
+    ||';'
 
 ;
 
