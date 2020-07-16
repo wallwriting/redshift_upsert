3c6c710cb8f3affd6f509ae20546360c0695aa57
wallwriting <hwang.john@gmail.com>
2020-07-16 15:33:31 -0400

    Update upsert_sp.sql
    Improved performance

--- upsert_sp.sql
+++ upsert_sp.sql
@@ -1,6 +1,14 @@
-CREATE OR REPLACE PROCEDURE sp_upsert(target_table IN CHARACTER VARYING, target_key IN CHARACTER VARYING, target_version IN CHARACTER VARYING, source_table IN CHARACTER VARYING, source_key IN CHARACTER VARYING, source_version IN CHARACTER VARYING, source_dml_indicator IN CHARACTER VARYING)
+CREATE OR REPLACE PROCEDURE sp_upsert(
+                                target_table IN CHARACTER VARYING, 
+                                target_key IN CHARACTER VARYING, 
+                                target_version IN CHARACTER VARYING, 
+                                source_table IN CHARACTER VARYING, 
+                                source_key IN CHARACTER VARYING, 
+                                source_version IN CHARACTER VARYING, 
+                                source_dml_indicator IN CHARACTER VARYING
+                                )
 --DROP PROCEDURE upsert_sp(target_table IN CHARACTER VARYING, target_key IN CHARACTER VARYING, target_version IN CHARACTER VARYING, source_table IN CHARACTER VARYING, source_key IN CHARACTER VARYING, source_version IN CHARACTER VARYING, source_dml_indicator IN CHARACTER VARYING)
- 
+
 AS $$
 DECLARE
     insert_var CHARACTER VARYING;
@@ -10,8 +18,27 @@ DECLARE
     colArrayVar CHARACTER VARYING;
     colArrayPrefixVar CHARACTER VARYING;
     finishInsertVar CHARACTER VARYING;
-    finishDeleteVar CHARACTER VARYING;
+--    tmpTableName CHARACTER VARYING;
+
 BEGIN
+
+-- tmpTableName = 
+--                 (
+--                 SELECT
+--                     'tmp_column_name_list_table_'
+--                     ||                    
+--                     REPLACE
+--                         (CAST(pg_backend_pid() AS VARCHAR(25)) 
+--                         || 
+--                         CAST
+--                             (CAST(EXTRACT('epoch' FROM current_timestamp) AS FLOAT)
+--                                 AS VARCHAR(35)), '.', ''
+--                         )
+--                 )
+-- ;
+
+
+
     /*Creates the temp table that will hold the column list. Table has arbitrary characters at the end
     to decrease the chance of duplicates. This value is not parameterized because it would require the
     queries to run AS an EXECUTE statement with quotes everywhere, like we are forced to do with the
@@ -76,9 +103,11 @@ BEGIN
         /*a second version of the column list that removes the table prefix*/
         colArrayVar = (SELECT REPLACE(colArrayPrefixVar, 'src.', ''));
 
-
 /*This ends the column list generating section*/
 
+
+
+/*hard codes the dml indiciator--this will eventually need to be flexible*/
 insert_var = 'I';
 update_var = 'U';
 
@@ -90,17 +119,17 @@ END IF;
 
 /*for the end of the delete statement, this will create different lines based on
 whether the call passed a real version id or the same value AS the key*/
-IF target_version = target_key THEN finishDeleteVar = '1=1';
-    ELSIF source_version = source_key THEN finishDeleteVar = '1=1';
-    ELSE finishDeleteVar = 'src.'||source_version || ' >= maxtgt.tgt_version_col';
-END IF;
+-- IF target_version = target_key THEN finishDeleteVar = '1=1';
+--     ELSIF source_version = source_key THEN finishDeleteVar = '1=1';
+--     ELSE finishDeleteVar = 'src.'||source_version || ' >= maxtgt.tgt_version_col';
+-- END IF;
 
 
 
 /*This starts the actual delete and insert*/
 EXECUTE
-/*deletes any existing rows that will change in the batch*/
-'DELETE
+'/*deletes any existing target rows that will change in the batch*/
+DELETE
 FROM
 	 ' || target_table || 
 ' WHERE 
@@ -108,73 +137,57 @@ FROM
     /*gets target table_key that have a version number lower than the source*/
     (
         SELECT
-            maxtgt.'||target_key ||
-        ' FROM
-            /*target table id and max version number*/
-            (
-                SELECT
-                     ' || target_key || ',
-                    MAX(' || target_version || ') AS tgt_version_col 
-                FROM
-                     ' || target_table ||
-                ' GROUP BY
-                     ' || target_key ||
-            ' ) maxtgt	
-      JOIN
-            /*source table*/
-             ' || source_table || ' src
-        ON 
-            src.'||source_key || ' = maxtgt.'||target_key ||
-            '/*only gets rows where the source timestamp is greater than the target timestamp*/
-            AND ' || 
-            finishDeleteVar
+            maxtgt.'||source_key ||
+         ' FROM
+            ' || source_table || ' as maxtgt'
 --'src.'||source_version || ' >= maxtgt.tgt_version_col'
     ||' )
 ;'
 ;
 
+
 EXECUTE
-'/*inserts source data into target table, only inserting new rows*/
-INSERT INTO ' || target_table ||
-    ' (' || colArrayVar || ') 
-     SELECT
-		/*replace this with whatever you need to insert*/
-		 ' || colArrayPrefixVar ||
-	' FROM
-		/*source*/
-		 ' || source_table || ' src
-	/*gets the most recent change for a given record
-    in case multiple updates were made at the source
-    in between batches*/
-	JOIN
-    		(
-			/*Three key elements: record identifier, versioning column, DML indicator*/
-          	SELECT
-				/*the record identifier*/
-          		  ' || target_key || ', 
-				/*most recent version*/
-          		MAX(' || target_version || ') AS tgt_version_col  
-          	FROM '
-          		 || source_table || ' src 
-			 WHERE 
-          		/*DML indcator, must filter out deletes prior to the GROUP BY*/ ' ||
-              finishInsertVar
---                'src.'||source_dml_indicator || ' IN(' || quote_literal(insert_var) || ', ' || quote_literal(update_var) || ')'
-          	|| ' GROUP BY '
-          		 || target_key || 
-        ') mx
-        ON src.'||source_key || ' = mx.'||target_key ||
-        ' AND src.'||source_version || ' = mx.tgt_version_col 
-    LEFT JOIN '
-         || target_table || ' tgt 
-        ON src.'||source_key || ' = tgt.'||target_key || 
-        ' AND src.'||source_version || ' = tgt.'||target_version || 
-    ' WHERE 
-        /*only inserts values not already present in target*/
-        tgt.'||target_key || ' IS NULL AND ' ||
-        finishInsertVar
---        'src.'||source_dml_indicator || ' IN(' || quote_literal(insert_var) || ', ' || quote_literal(update_var) || ')'
-    ||';'
+    '/*In cases where there are multiple transactions in the incoming batch
+    this will delete all but the latest record*/
+    DELETE FROM ' || source_table || '
+    WHERE EXISTS
+        (     
+                SELECT 1
+                FROM
+                    /*This finds the max version number for each id*/
+                    (
+                        SELECT
+                            src.' || source_key || ', 
+                            MAX(src.' || source_version || ') AS max_version_col  
+                        FROM '
+                            || source_table || ' src 
+                        GROUP BY '
+                            || source_key ||  ' 
+                    ) mx
+                  WHERE ' 
+                      || source_table || '.' || source_key || ' = mx.' || source_key || '  
+                      /*deletes anything that is not the max version number*/
+                      AND ' || source_table || '.' || source_version || ' != mx.max_version_col 
+        )
+    ;'
+
+;
+
+
+EXECUTE
+    '/*inserts source data into target table, only inserting new rows*/
+    INSERT INTO ' || target_table || '
+        (' || colArrayVar || ') 
+        SELECT 
+        /*replace this with whatever you need to insert*/ '
+        || colArrayPrefixVar || ' 
+        FROM
+        /*source*/ ' 
+        || source_table || ' src
+        WHERE 
+            /*only inserts inserts and updates*/ ' 
+            || finishInsertVar || ' 
+    ;'
 
 ;
 
